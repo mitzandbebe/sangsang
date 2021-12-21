b4859e376f131dd4b7de62ca5ed88cbb3acf8e9a
mitzandbebe <hwaseo0314@naver.com>
2021-12-21 19:41:00 +0900

    강유빈이 지금 커밋했음

--- sangsanggongbang/src/main/java/com/gr/ssgb/test/HelloController.java
+++ sangsanggongbang/src/main/java/com/gr/ssgb/test/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 
-		model.addAttribute("name", "김철수 ");
+		model.addAttribute("name", "강유빈");
 		return "thymeleaf/hello";
 
 		// => http://localhost:9091/demo/hello
