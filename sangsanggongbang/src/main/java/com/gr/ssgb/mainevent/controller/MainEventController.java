d3cec3f866181b437cfdefd23ce865733c2a0241
uujploy <jung9201@gmail.com>
2021-12-23 16:53:00 +0900

    이벤트수정중,,

--- sangsanggongbang/src/main/java/com/gr/ssgb/mainevent/controller/MainEventController.java
+++ sangsanggongbang/src/main/java/com/gr/ssgb/mainevent/controller/MainEventController.java
@@ -1,13 +1,21 @@
 package com.gr.ssgb.mainevent.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gr.ssgb.mainevent.model.MainEventService;
+import com.gr.ssgb.mainevent.model.MainEventVO;
 
 @Controller
 @RequestMapping("/mainevent")
@@ -35,6 +43,24 @@ public class MainEventController {
 		return "mainevent/eventwrite";
 	}
 	
+	@PostMapping("/eventwrite")
+	public String write_post(@ModelAttribute MainEventVO vo,HttpServletRequest request) {
+		logger.info("이벤트 등록 처리,파라미터 vo ={}",vo);
+		
+		int cnt = mainEventService.insertEvent(vo);
+		logger.info("등록 결과, cnt={}",cnt);
+		
+		return "redirect:/mainevent/eventlist";
+	}
 	
-	
+	@RequestMapping("/eventlist")
+	public String list(Model model) {
+		logger.info("이벤트 목록");
+		
+		List<MainEventVO> list=mainEventService.selectAll();
+		
+		model.addAttribute("list",list);
+		
+		return "/mainevent/eventlist";
+	}
 }
