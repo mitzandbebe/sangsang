4144bd5605cae7a2cbf49bacb338b69a481dd5d3
mitzandbebe <hwaseo0314@naver.com>
2021-12-30 09:54:19 +0900

    으아

---
+++ sangsanggongbang/src/main/webapp/resources/assets/js/balancing.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+ 
+ 
+ $(function() {
+	$('#tabs-text-1').ready(function(){
+	$('#list1').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2");
+	});
+	
+	$('#tabs-text-2').ready(function(){
+		 $('#list2').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?searchCondition=b_flag&searchKeyword=N");
+	});
+	$('#tabs-text-3').ready(function(){
+		 $('#list3').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?searchCondition=b_flag&searchKeyword=Y");
+	});
+	
+	$('#pagelinknum2:first').click(function(){
+		location.href='http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?currentPage=2=&searchCondition=b_flag&searchKeyword=N';
+	});
+});
+
+
