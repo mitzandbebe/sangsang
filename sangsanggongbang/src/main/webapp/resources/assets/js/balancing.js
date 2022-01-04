eec88dd265a4db6469814def10d2df1a33ea4835
JHoon1321 <wlddj22@gmail.com>
2022-01-04 18:03:05 +0900

    업뎃

--- sangsanggongbang/src/main/webapp/resources/assets/js/balancing.js
+++ sangsanggongbang/src/main/webapp/resources/assets/js/balancing.js
@@ -1,23 +1,20 @@
 /**
- * 정산신청 탭 게시판 로드
+ * 정산신청 탭 게시판 로드 업뎃!!
  */
  
  
- $(function() {
+$(function() {
 	$('#tabs-text-1').ready(function(){
-	$('#list1').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2");
+	$('#list1').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list");
 	});
 	
 	$('#tabs-text-2').ready(function(){
-		 $('#list2').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?searchCondition=b_flag&searchKeyword=N");
+		 $('#list2').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list?searchCondition=b_flag&searchKeyword=N");
 	});
 	$('#tabs-text-3').ready(function(){
-		 $('#list3').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?searchCondition=b_flag&searchKeyword=Y");
+		 $('#list3').load("http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list?searchCondition=b_flag&searchKeyword=Y");
 	});
-	
-	$('#pagelinknum2:first').click(function(){
-		location.href='http://localhost:9091/sangsanggongbang/dashboard/host/balancing/list2?currentPage=2=&searchCondition=b_flag&searchKeyword=N';
-	});
+
 });
 
 
