5cb0ccaefb4e6db66b4fdb6b70747f7d8978fb19
boxgom <qkqkdldy@naver.com>
2022-01-08 13:58:51 +0900

    어머나 세상에?

--- sangsanggongbang/src/main/java/com/gr/ssgb/common/TopCount.java
+++ sangsanggongbang/src/main/java/com/gr/ssgb/common/TopCount.java
@@ -24,19 +24,17 @@ public class TopCount {
 		this.memberService = memberService;
 	}
 	
-	@GetMapping(value = {"/index"}) // 여기다 다중 매핑으로 모든 페이지 메핑을 해주면 됩니다!
-	public String noteNum(HttpSession session,HttpServletRequest request ,Model model) {
-		String mId = (String) session.getAttribute("mId"); // 쪽지 확인을 위해
-		logger.info("mId={}", mId);
-		StringBuffer urlTem = request.getRequestURL(); // 해당 페이지의 url을 알기위해
-		String url = urlTem.substring(39); // 리턴을 위한 url추출
-		logger.info("url={}",url);
-		
-		int num = noteService.readNum(mId);
-		logger.info("안읽은 쪽지 개수 num={}", num);
-
-		
-		model.addAttribute("noteNum", num);
-		return url;
-	}
+	/*
+	 * @GetMapping(value = {"/index"}) // 여기다 다중 매핑으로 모든 페이지 메핑을 해주면 됩니다! public
+	 * String noteNum(HttpSession session,HttpServletRequest request ,Model model) {
+	 * String mId = (String) session.getAttribute("mId"); // 쪽지 확인을 위해
+	 * logger.info("mId={}", mId); StringBuffer urlTem = request.getRequestURL(); //
+	 * 해당 페이지의 url을 알기위해 String url = urlTem.substring(39); // 리턴을 위한 url추출
+	 * logger.info("url={}",url);
+	 * 
+	 * int num = noteService.readNum(mId); logger.info("안읽은 쪽지 개수 num={}", num);
+	 * 
+	 * 
+	 * model.addAttribute("noteNum", num); return url; }
+	 */
 }
