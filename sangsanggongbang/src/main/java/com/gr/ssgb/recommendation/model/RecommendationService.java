ae4f1e17bdb46d7ddd87bde27e901108eb6b2878
VIP <VIP@DESKTOP-3KRO6UI>
2022-01-08 19:10:10 +0900

    불편사항 게시판

--- sangsanggongbang/src/main/java/com/gr/ssgb/recommendation/model/RecommendationService.java
+++ sangsanggongbang/src/main/java/com/gr/ssgb/recommendation/model/RecommendationService.java
@@ -1,18 +1,21 @@
 package com.gr.ssgb.recommendation.model;
 
+import java.util.List;
+
+import com.gr.ssgb.common.SearchVO;
+
 public interface RecommendationService {
-	
+
 	public int insertRecommendation(RecommendationVO vo);
 
-	/*
-	 * List<RecommendationVO> selectAllRecommendation(SearchVO searchVo);
-	 * 
-	 * public RecommendationVO selectByNoRecommendation(int recoNo);
-	 * 
-	 * int selectTotalRecord(SearchVO searchVo);
-	 * 
-	 * public int updateRecommendation(RecommendationVO vo);
-	 * 
-	 * public int deleteRecommendation(int recoNo);
-	 */
+	List<RecommendationVO> selectAllRecommendation(SearchVO searchVo);
+
+	public RecommendationVO selectByNoRecommendation(int recoNo);
+
+	int selectTotalRecord(SearchVO searchVo);
+
+	public int updateRecommendation(RecommendationVO vo);
+
+	public int deleteRecommendation(int recoNo);
+
 }
