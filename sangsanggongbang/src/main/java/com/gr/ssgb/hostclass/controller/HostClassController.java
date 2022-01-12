ddf829b2662f185fe525bc3849ac689327974fa0
uujploy <89951507+uujploy@users.noreply.github.com>
2022-01-12 11:22:06 +0900

    클래스 안들어가는거 수정했습니다.

--- sangsanggongbang/src/main/java/com/gr/ssgb/hostclass/controller/HostClassController.java
+++ sangsanggongbang/src/main/java/com/gr/ssgb/hostclass/controller/HostClassController.java
@@ -11,11 +11,13 @@ import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +58,36 @@ public class HostClassController {
 		this.hostService = hostService;
 		this.memberService = memberService;
 		logger.info("클래스 생성자 주입");
+	}
+
+	@GetMapping(value = "/search")
+	public String getSearchClassPage(HttpServletRequest request,
+									 @ModelAttribute("search") HostClassVO hostClassVO,
+									 ModelMap model) {
+
+		List<String> area = ImmutableList.<String>builder()
+				.add("서울")
+				.add("경기")
+				.add("인천")
+				.add("강원")
+				.add("충청")
+				.add("세종")
+				.add("전라")
+				.add("경상")
+				.add("제주")
+				.build();
+
+		List<CategoryVO> clist = hostClassService.selectCategoryAll();
+
+
+		List<HostClassVO> list = this.hostClassService.findBySearchClass(hostClassVO);
+
+
+		model.addAttribute("clist", clist);
+		model.addAttribute("area", area);
+		model.addAttribute("list", list);
+
+		return "class/search";
 	}
 
 	@GetMapping("/inputclass")
@@ -248,10 +280,16 @@ public class HostClassController {
 		int reviewCnt = reviewService.selectReviewByHost(hNo);
 		
 		String mId=(String) session.getAttribute("mId");
-		int mNo=memberService.selectMno(mId);
-		concern.setmNo(mNo);
-		concern.setcNo(cNo);
-		int con=hostClassService.selectConcernbyCNo(concern);
+		try{
+			int mNo=memberService.selectMno(mId);
+			concern.setmNo(mNo);
+			concern.setcNo(cNo);
+			int con=hostClassService.selectConcernbyCNo(concern);
+			model.addAttribute("con",con);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
 		model.addAttribute("classlist",classlist);
 		model.addAttribute("avgRate", avgRate);
@@ -259,7 +297,7 @@ public class HostClassController {
 		model.addAttribute("hostVo", vo);
 		model.addAttribute("classCnt", classCnt);
 		model.addAttribute("reviewCnt", reviewCnt);
-		model.addAttribute("con",con);
+		model.addAttribute("mId",mId);
 		
 		
 		return "class/detail";
