d15c32546eb98fa0ab4f8be38ad348440259b105
mitzandbebe <hwaseo0314@naver.com>
2022-01-18 13:44:34 +0900

    유효성 수정 완료

--- sangsanggongbang/src/main/webapp/resources/dbsql/CATEGORY.sql
+++ sangsanggongbang/src/main/webapp/resources/dbsql/CATEGORY.sql
@@ -10,3 +10,28 @@ ALTER TABLE CATEGORY
 		PRIMARY KEY (
 			CATEGORY_CODE
 		);
+
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'공예DIY');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'베이킹');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'쿠킹');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'문화예술');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'자기계발');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'뷰티');
+insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'여행');
+ insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'피트니스');
+ insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'스포츠');
+ insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'모임');
+ insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'스토어');
+ insert into category (category_code,category_name)
+ values(CATEGORY_SEQ.nextval,'온라인');
